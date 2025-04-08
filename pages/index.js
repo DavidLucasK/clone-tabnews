a0d1c0d95eeb6fae2ae77a1daafd58a8132fbd71
David Lucas de Freitas Andrade <55153395+DavidLucasK@users.noreply.github.com>
2025-04-08 13:27:43 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
-    return(
-        <div>
-            <h1>Pimbinha legal demais</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Pimbinha legal demais</h1>
+    </div>
+  );
 }
 
 export default Home;
