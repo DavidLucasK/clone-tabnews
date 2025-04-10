7e231c0a0a62e5c89188a53ba5f5c8cb07fb96ce
David Lucas de Freitas Andrade <55153395+DavidLucasK@users.noreply.github.com>
2025-04-10 00:39:11 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  console.log(`o status é: ${response.status}`);
-  console.log(`a response é: ${response}`);
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  //Retornar a data corretamente
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(Date.parse(responseBody.updated_at)).not.toEqual(NaN);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.8");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+
+  //Não conter senha ou email no corpo da resposta
+  expect(responseBody).not.toHaveProperty("password");
+  expect(responseBody).not.toHaveProperty("email");
 });
