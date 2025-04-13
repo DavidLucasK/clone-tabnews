b3f48f470006092a5dff3eff9687fb1ea6042a76
David Lucas de Freitas Andrade <55153395+DavidLucasK@users.noreply.github.com>
2025-04-13 04:18:50 +0000

    improving `wait-for-postgres` scrript to input some cool animation

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,19 +1,38 @@
 const { exec } = require("node:child_process");
 
+let intervaloBolinhas;
+let indiceBolinhas = 0;
+// Loading de bolinhas (mais daora)
+const bolinhas = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+
+function loadingMassinha() {
+  intervaloBolinhas = setInterval(() => {
+    const frame = bolinhas[indiceBolinhas++ % bolinhas.length];
+    process.stdout.write(
+      `\r\x1b[33m${frame} Aguardando Postgres aceitar conexões...\x1b[0m `,
+    );
+  }, 100);
+}
+
+function stopLoading() {
+  clearInterval(intervaloBolinhas);
+  process.stdout.write(
+    `\r\x1b[32m✔ Postgres está pronto e aceitando conexões!\x1b[0m\n`,
+  );
+}
+
 function checkPostgres() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
   function handleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
-      process.stdout.write(".");
-      checkPostgres();
+      setTimeout(checkPostgres, 500); // Tenta novamente em 500ms
       return;
     }
 
-    console.log("\n🟢Postgres está pronto e aceitando conexões!");
+    stopLoading();
   }
 }
 
-process.stdout.write("\n\n🔴Aguardando Postgres aceitar conexões");
-
+loadingMassinha();
 checkPostgres();
