2e9a8c722d4ec0c2737c30c80f6f988c01db7e93
David Lucas de Freitas Andrade <55153395+DavidLucasK@users.noreply.github.com>
2025-04-18 23:27:11 +0000

    refactor: changing join to resolve in dirDatabase migrations and changing toBeGreaterThanOr= test

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import { resolve } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
@@ -15,14 +15,10 @@ export default async function migrations(request, response) {
   try {
     dbClient = await database.getNewClient();
 
-    const migrationsDir =
-      process.env.NODE_ENV === "production"
-        ? resolve("infra", "migrations")
-        : join("infra", "migrations");
     const defaultMigrationOptions = {
       dbClient: dbClient,
       dryRun: true,
-      dir: migrationsDir,
+      dir: resolve("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
